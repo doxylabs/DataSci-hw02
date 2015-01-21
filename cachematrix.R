0aba6663251c8d2aea89133940f44e5e1fb07049
jrherrick <jrherrick@users.noreply.github.com>
2015-01-21 15:17:35 -0800

    initial commits...

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix inversion, being a costly operation, is a great candidate for result
+# caching. Here we have a couple of functions responsible for doing so.
+#       -- Jason Herrick January 2015
 
-## Write a short comment describing this function
+# makeCacheMatrix --------------------------------------------------------- 
 
+# This section contains the function makeCacheMatrix, a factory responsible for 
+# creating and returning a vector with decorator functions to:
+
+# 1. solve : Return the inverse of a supplied matrix, storing and calculating 
+#            it the first time or if marked dirty, returning it on
+#            subsequent calls.
 makeCacheMatrix <- function(x = matrix()) {
 
 }
@@ -12,4 +19,5 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    solve
 }
