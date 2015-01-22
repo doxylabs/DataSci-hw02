fed7c429dab1c45f333d21c1876e89d40413e83d
jrherrick <jrherrick@users.noreply.github.com>
2015-01-22 10:09:07 -0800

    Added a gitignore and initial fumblings with cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,18 +6,31 @@
 
 # This section contains the function makeCacheMatrix, a factory responsible for 
 # creating and returning a vector with decorator functions to:
-
-# 1. solve : Return the inverse of a supplied matrix, storing and calculating 
-#            it the first time or if marked dirty, returning it on
-#            subsequent calls.
+# solve : Return the inverse of a supplied matrix, storing and calculating 
+#         it the first time or if marked dirty, returning it on
+#         subsequent calls.
 makeCacheMatrix <- function(x = matrix()) {
-
+    set<-function(y=matrix()){
+        x <<- y # the parent value of x is set with set(y)
+        m <<- NULL
+    }
+    get<-function() x # return x
+    list(set=set,
+         get=get
+         )
 }
 
+x<-c(c(1,0,0),c(0,1,0),c(0,0,1))
+mat<-makeCacheMatrix(x)
+mat$get()
+mat[["get"]]()
+for( i in ls(mat) )    mat[[i]]()
 
-## Write a short comment describing this function
+# cacheSolve --------------------------------------------------------------
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     solve
 }
+
+
