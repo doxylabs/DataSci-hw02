e876d312def3f347462a2d40e5a9a997830bccac
jrherrick <jrherrick@users.noreply.github.com>
2015-01-22 11:08:02 -0800

    closure is not subsettable? I'll subset ya'

--- cachematrix.R
+++ cachematrix.R
@@ -2,35 +2,54 @@
 # caching. Here we have a couple of functions responsible for doing so.
 #       -- Jason Herrick January 2015
 
+library(digest)
+
 # makeCacheMatrix --------------------------------------------------------- 
+
 
 # This section contains the function makeCacheMatrix, a factory responsible for 
 # creating and returning a vector with decorator functions to:
 # solve : Return the inverse of a supplied matrix, storing and calculating 
 #         it the first time or if marked dirty, returning it on
 #         subsequent calls.
+
 makeCacheMatrix <- function(x = matrix()) {
-    set<-function(y=matrix()){
+    hsh<-digest(NULL) # store the hash of the matrix passed to me in case it changes
+#     dir(hsh)
+#     h=digest("abc")
+#     if(hsh!=h) print("ok")
+    set<-function(y=matrix())
         x <<- y # the parent value of x is set with set(y)
-        m <<- NULL
-    }
-    get<-function() x # return x
+    
+    get<-function()        x # return x
+    
+    getHash<-function()        return(hsh) # return the hash
+
+    setHash<-function(h=character())        hsh <<- h
+    
     list(set=set,
-         get=get
-         )
+         get=get,
+         getHash=getHash,
+         setHash=setHash
+    )
 }
 
-x<-c(c(1,0,0),c(0,1,0),c(0,0,1))
-mat<-makeCacheMatrix(x)
-mat$get()
-mat[["get"]]()
-for( i in ls(mat) )    mat[[i]]()
 
 # cacheSolve --------------------------------------------------------------
 
+
+# This function computes the inverse of the special "matrix" returned by
+# makeCacheMatrix above. If the inverse has already been calculated (and the
+# matrix has not changed), then cacheSolve should retrieve the inverse from the
+# cache.
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    solve
+    if( makeCacheMatrix$getHash() != digest(x) )
+    {
+        makeCacheMatrix$setHash(digest(x))
+        makeCacheMatrix$set(solve(x))
+    }
+    return (makeCacheMatrix$get())
 }
 
 
